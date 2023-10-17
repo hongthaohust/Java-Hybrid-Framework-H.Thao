02d9d291393592dd9d98cd65a8601b2a4d7c636a
thao_pham <phamthihongthao241198@gmail.com>
2023-10-17 18:02:03 +0700

    multiple browser

--- actions/commons/AbstractTest.java
+++ actions/commons/AbstractTest.java
@@ -1,5 +1,62 @@
 package commons;
 
-public class AbstractTest {
+import java.time.Duration;
+import java.util.Random;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class AbstractTest {
+	private WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+	
+//	if (osName.contains("Windows")) {
+//	System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+//} else {
+//	System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+//}
+	
+	protected WebDriver getBrowserDriver(String browserName) {
+		if(browserName.equals("firefox_ui")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else if(browserName.equals("chrome_ui")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if(browserName.equals("firefox_headless")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			FirefoxOptions options = new FirefoxOptions();
+			options.addArguments("--headless=new");
+			options.addArguments("window-size=1920×1080");
+			driver = new FirefoxDriver(options);
+		} else if(browserName.equals("chrome_headless")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--headless=new");
+			options.addArguments("window-size=1920×1080");
+			driver = new ChromeDriver(options);
+		} else if(browserName.equals("edge_chromium")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+			driver = new EdgeDriver();
+		} else {
+			throw new RuntimeException("Please input browser value");
+		}
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().window().maximize();
+		driver.get("https://demo.nopcommerce.com/");
+		
+		return driver;
+	}
+	
+	public int random() {
+		Random rand = new Random();
+		return rand.nextInt(999999);
+	}
+	
 }
--- testcases/com/nopcommerce/users/Level_04_Register_Login_User_Multiple_Browser.java
+++ testcases/com/nopcommerce/users/Level_04_Register_Login_User_Multiple_Browser.java
@@ -2,72 +2,31 @@ package com.nopcommerce.users;
 
 import org.testng.annotations.Test;
 
+import commons.AbstractTest;
 import pageObjects.CustomerInfoPageObject;
 import pageObjects.HomePageObject;
 import pageObjects.LoginPageObject;
 import pageObjects.RegisterPageObject;
 
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
-import java.time.Duration;
 import java.util.Random;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
-public class Level_04_Register_Login_User_Multiple_Browser {
+public class Level_04_Register_Login_User_Multiple_Browser extends AbstractTest {
 	WebDriver driver;
 	Select select;
-	String projectPath = System.getProperty("user.dir");
-	String osName = System.getProperty("os.name");
 	String firstName, lastName, dayOfBirth, monthOfBirth, yearOfBirth, email, companyName, password;
 	
 	@Parameters("browser")
 	@BeforeClass
 	public void beforeClass(String browserName) {
-//		if (osName.contains("Windows")) {
-//			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-//		} else {
-//			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
-//		}
-		
-		if(browserName.equals("firefox_ui")) {
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-			driver = new FirefoxDriver();
-		} else if(browserName.equals("chrome_ui")) {
-			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
-			driver = new ChromeDriver();
-		} else if(browserName.equals("firefox_headless")) {
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-			FirefoxOptions options = new FirefoxOptions();
-			options.addArguments("--headless=new");
-			options.addArguments("window-size=1920×1080");
-			driver = new FirefoxDriver(options);
-		} else if(browserName.equals("chrome_headless")) {
-			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
-			ChromeOptions options = new ChromeOptions();
-			options.addArguments("--headless=new");
-			options.addArguments("window-size=1920×1080");
-			driver = new ChromeDriver(options);
-		} else if(browserName.equals("edge_chromium")) {
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
-			driver = new EdgeDriver();
-		} else {
-			throw new RuntimeException("Please input browser value");
-		}
-
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-		driver.manage().window().maximize();
-		driver.get("https://demo.nopcommerce.com/");
+		driver = getBrowserDriver(browserName);
 		
 		firstName = "MyFirstName";
 		lastName = "MyLastName";
@@ -140,11 +99,6 @@ public class Level_04_Register_Login_User_Multiple_Browser {
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
-	}
-	
-	public int random() {
-		Random rand = new Random();
-		return rand.nextInt(999999);
 	}
 	
 	HomePageObject homePage;
