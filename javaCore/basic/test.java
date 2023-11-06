3fe179b96782adb1443ae6ededc16495cf8aa2fc
thao_pham <phamthihongthao241198@gmail.com>
2023-11-06 14:30:55 +0700

    test java

--- javaCore/basic/test.java
+++ javaCore/basic/test.java
@@ -3,7 +3,17 @@ package basic;
 public class test {
 
 	public static void main(String[] args) {
-		print_charactor_N(5);
+//		for(int i = 2; i< 10; i++) {
+//			for (int x= 1; x<= 10; x++) {
+//				System.out.println(i + " * " + x + " = " + x*i);
+//			}
+//		}
+//		
+//		print_charactor_N(10);
+		
+		print_charactor_H(0);
+		print_charactor_H(2);
+		print_charactor_H(5);
 	}
 	
 	public static void print_charactor_N(int y) {
@@ -20,6 +30,31 @@ public class test {
 				}
 				System.out.println("");
 			}
+		}
+		
+	}
+	
+	public static void print_charactor_H(int y) {
+		if(y>2 && y%2==1 ) {
+			for(int i = 0; i< y; i++) {
+				if(i == (y/2)) {
+					for(int j = 0; j< y; j++) {
+						System.out.print("H");
+					}
+				}
+				else {
+					for(int j = 0; j< y; j++) {
+						if(j == 0 || j == y-1) {
+							System.out.print("H");
+						}
+						else System.out.print("O");
+					}
+				}
+				System.out.println("");
+			}
+		}
+		else {
+			System.out.println("Nhập vào số lẻ >= 3");
 		}
 		
 	}
